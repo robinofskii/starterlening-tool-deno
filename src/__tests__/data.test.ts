63c07bd49340e8808c7b960d7c07b278151987aa
Robinofskii <robinofski.development@gmail.com>
2024-10-18 21:55:23 +0200

    test: add tests to project
    use deno's testing tooling to test all functions currently in repo. Coverage returns 100% at this commit

---
+++ src/__tests__/data.test.ts
@@ -0,0 +1,18 @@
+import { expect } from "@std/expect";
+
+import { normalizeString } from "../helpers/data.ts";
+
+Deno.test("normalizeString should return a string with no leading or trailing whitespace", () => {
+    const actual = normalizeString("  hello  ");
+    const expected = "hello";
+
+    expect(actual).toEqual(expected);
+});
+
+Deno.test("normalizeString should return a string with all lowercase characters", () => {
+    const actual = normalizeString("HeLLo");
+    const expected = "hello";
+
+    expect(actual).toEqual(expected);
+});
+
---
+++ src/__tests__/read-csv.test.ts
@@ -0,0 +1,35 @@
+import { expect } from "@std/expect";
+
+import { loadCbsCsv, loadSVnCsv } from "../helpers/read-csv.ts";
+
+import type { ParsedSvnDataEntry, ParsedCbsDataEntry } from "../types/index.ts";
+
+Deno.test("loadSVnCsv should load and parse SVn CSV data correctly", async () => {
+    const data = await loadSVnCsv("./src/data/datasheet.csv");
+    expect(Array.isArray(data)).toEqual(true);
+    if (data.length > 0) {
+        const entry: ParsedSvnDataEntry = data[0];
+        expect(typeof entry.municipality).toEqual("string");
+        expect(typeof entry.loan).toEqual("string");
+    }
+});
+
+Deno.test("loadCbsCsv should load and parse CBS CSV data correctly", async () => {
+    const data = await loadCbsCsv("./src/data/woonplaatsen_nederland_2024.csv");
+
+    expect(Array.isArray(data)).toEqual(true);
+    if (data.length > 0) {
+        const entry: ParsedCbsDataEntry = data[0];
+        expect(typeof entry.municipality).toEqual("string");
+        expect(typeof entry.state).toEqual("string");
+        expect(typeof entry.partOfCountry).toEqual("string");
+    }
+});
+
+Deno.test("loadSVnCsv should throw an error when the file does not exist", async () => {
+    try {
+        await loadSVnCsv("./src/data/nonexistent.csv");
+    } catch (error) {
+        expect(error).toBeDefined();
+    }
+});
