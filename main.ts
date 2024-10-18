6e92f3fe66432468f77671ae8b539991aa67b3c5
Robinofskii <robinofski.development@gmail.com>
2024-10-18 22:07:01 +0200

    style: format repo with `deno fmt`

--- main.ts
+++ main.ts
@@ -2,7 +2,9 @@ import { loadCbsCsv, loadSVnCsv, mergeData } from "./src/helpers/index.ts";
 
 try {
   const SvnData = await loadSVnCsv("./src/data/datasheet.csv");
-  const CbsData = await loadCbsCsv("./src/data/woonplaatsen_nederland_2024.csv");
+  const CbsData = await loadCbsCsv(
+    "./src/data/woonplaatsen_nederland_2024.csv",
+  );
 
   mergeData(SvnData, CbsData);
 } catch (error) {
