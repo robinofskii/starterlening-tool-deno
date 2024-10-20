a4a49bc890db7871cc35802a94dd77873fdb7ebc
Robinofskii <robinofski.development@gmail.com>
2024-10-20 23:25:08 +0200

    feat: Add database helper files for PostgreSQL
    Also include a new task to populate Postgres DB with CSV data

---
+++ src/helpers/db/client.ts
@@ -0,0 +1,10 @@
+import postgres from "https://deno.land/x/postgresjs@v3.4.4/mod.js";
+import "@std/dotenv/load";
+
+export const postgresClient = postgres({
+    database: Deno.env.get("DB_DATABASE"),
+    hostname: Deno.env.get("DB_HOST"),
+    port: parseInt(Deno.env.get("DB_PORT") as string),
+    user: Deno.env.get("DB_USER"),
+    password: Deno.env.get("DB_PASSWORD"),
+})
---
+++ src/helpers/db/populate.ts
@@ -0,0 +1,23 @@
+import { loadCbsCsv, loadSVnCsv, mergeData } from "../index.ts";
+import { postgresClient } from "./client.ts";
+
+// Populate the database with the data from the CSV files
+// This file should be called only with the deno task
+try {
+    const SvnData = await loadSVnCsv("./src/data/datasheet.csv");
+    const CbsData = await loadCbsCsv(
+        "./src/data/woonplaatsen_nederland_2024.csv",
+    );
+
+    const mergedData = mergeData(SvnData, CbsData);
+
+    // Empty the municipalities table before inserting new data
+    await postgresClient`TRUNCATE municipalities`;
+
+    // Insert the merged data into the municipalities table
+    await postgresClient`
+    INSERT INTO municipalities ${postgresClient(mergedData)}
+    `;
+} catch (error) {
+    console.error("An error occurred:", error);
+}
