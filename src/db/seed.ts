06de2d37093c11e5730f296222f423a9bdc0a22b
Robinofskii <robinofski.development@gmail.com>
2024-10-26 22:25:55 +0200

    feat: Add functionality to seed the municipalities table with data from a CSV file

---
+++ src/db/seed.ts
@@ -0,0 +1,16 @@
+import { loadSVnCsv } from "../utils/index.ts";
+import { postgresClient } from "./client.ts";
+
+try {
+    const SvnData = await loadSVnCsv("./src/data/datasheet.csv");
+
+    // Empty the municipalities table before inserting new data
+    await postgresClient`TRUNCATE municipalities`;
+
+    // Insert the merged data into the municipalities table
+    await postgresClient`
+    INSERT INTO municipalities ${postgresClient(SvnData)}
+    `;
+} catch (error) {
+    console.error("An error occurred:", error);
+}
