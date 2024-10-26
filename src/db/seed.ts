3c24995fb5c90006a859e0dbf8e39cd2f448c2a7
Robinofskii <robinofski.development@gmail.com>
2024-10-26 22:33:12 +0200

    style: format repo with `deno fmt`

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -2,15 +2,15 @@ import { loadSVnCsv } from "../utils/index.ts";
 import { postgresClient } from "./client.ts";
 
 try {
-    const SvnData = await loadSVnCsv("./src/data/datasheet.csv");
+  const SvnData = await loadSVnCsv("./src/data/datasheet.csv");
 
-    // Empty the municipalities table before inserting new data
-    await postgresClient`TRUNCATE municipalities`;
+  // Empty the municipalities table before inserting new data
+  await postgresClient`TRUNCATE municipalities`;
 
-    // Insert the merged data into the municipalities table
-    await postgresClient`
+  // Insert the merged data into the municipalities table
+  await postgresClient`
     INSERT INTO municipalities ${postgresClient(SvnData)}
     `;
 } catch (error) {
-    console.error("An error occurred:", error);
+  console.error("An error occurred:", error);
 }
