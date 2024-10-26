ccc1041fb65e91a80e5f37d84c19e90a9e81974a
Robinofskii <robinofski.development@gmail.com>
2024-10-26 23:02:41 +0200

    style: format new test

--- src/__tests__/timing.test.ts
+++ src/__tests__/timing.test.ts
@@ -4,27 +4,27 @@ import { FakeTime } from "@std/testing/time";
 import { delay, randomDelay } from "../utils/timing.ts";
 
 Deno.test("delay should resolve after the specified time", () => {
-    using time = new FakeTime();
-    const delayTime = 1000;
-    const start = Date.now();
+  using time = new FakeTime();
+  const delayTime = 1000;
+  const start = Date.now();
 
-    delay(delayTime);
-    time.tick(delayTime);
+  delay(delayTime);
+  time.tick(delayTime);
 
-    const end = Date.now();
-    expect(end - start).toBeGreaterThanOrEqual(delayTime);
+  const end = Date.now();
+  expect(end - start).toBeGreaterThanOrEqual(delayTime);
 });
 
 Deno.test("randomDelay should resolve after a random time", () => {
-    using time = new FakeTime();
-    const min = 1000;
-    const max = 2000;
-    const start = Date.now();
+  using time = new FakeTime();
+  const min = 1000;
+  const max = 2000;
+  const start = Date.now();
 
-    randomDelay(min, max);
-    time.tick(max);
+  randomDelay(min, max);
+  time.tick(max);
 
-    const end = Date.now();
-    expect(end - start).toBeGreaterThanOrEqual(min);
-    expect(end - start).toBeLessThanOrEqual(max);
+  const end = Date.now();
+  expect(end - start).toBeGreaterThanOrEqual(min);
+  expect(end - start).toBeLessThanOrEqual(max);
 });
